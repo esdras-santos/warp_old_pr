942e0de36de7bb65561175419db97ab8849951a5
esdras santos <esantoz@protonmail.com>
2023-04-06 12:16:05 -0300

    old pr

---
+++ builtinHandler/keccak.ts
@@ -0,0 +1,34 @@
+import { DataLocation, ExternalReferenceType, FunctionCall } from 'solc-typed-ast';
+import { AST } from '../../ast/ast';
+import { ASTMapper } from '../../ast/mapper';
+import { createCallToFunction } from '../../utils/functionGeneration';
+import {
+  createArrayTypeName,
+  createBytesNTypeName,
+  createUintNTypeName,
+} from '../../utils/nodeTemplates';
+
+export class Keccak extends ASTMapper {
+  visitFunctionCall(node: FunctionCall, ast: AST): void {
+    if (
+      !(
+        node.vFunctionName === 'keccak256' &&
+        node.vFunctionCallType === ExternalReferenceType.Builtin
+      )
+    ) {
+      return this.commonVisit(node, ast);
+    }
+
+    const warpKeccak = ast.registerImport(
+      node,
+      'warplib.keccak',
+      'warp_keccak',
+      [['input', createArrayTypeName(createUintNTypeName(8, ast), ast), DataLocation.Memory]],
+      [['hash', createBytesNTypeName(32, ast)]],
+    );
+
+    ast.replaceNode(node, createCallToFunction(warpKeccak, node.vArguments, ast));
+
+    this.commonVisit(node, ast);
+  }
+}
